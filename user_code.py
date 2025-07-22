ac95cd496e7ea24f0058cff60c4cf13d9785e538
Chikkl <pavel.schershnev@yandex.ru>
2025-07-22 17:10:20 +0300

    fix: delete file for functional test

--- user_code.py
+++
@@ -1,4 +0,0 @@
-async def process_data(data):
-    # Фильтруем четные числа, возводим их в квадрат и суммируем
-    sum_of_squares = sum(x ** 2 for x in data if x % 2 == 0)
-    return sum_of_squares
